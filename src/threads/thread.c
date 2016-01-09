debb5f72a84b4f021eadfb2d8626b25d3faa6668
Franky Romero <faromero@usc.edu>
2016-01-08 18:18:04 -0800

    test commit

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -15,6 +15,8 @@
 #include "userprog/process.h"
 #endif
 
+// Franky Test
+
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
    of thread.h for details. */
