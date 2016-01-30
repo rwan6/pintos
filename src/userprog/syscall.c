0d79c3768414c29aa72cb1bd1906096a0d28ba0e
Franky Romero <faromero@usc.edu>
2016-01-30 13:18:10 -0800

    Initial stubs syscall

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h" /* For shutdown_power_off. */
 
 static void syscall_handler (struct intr_frame *);
 
@@ -18,3 +19,36 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+/* Terminates Pintos. */
+static void
+halt (void)
+{
+  shutdown_power_off();
+}
+
+/* Terminates the current user program.
+   Status = 0 indicates success.
+   Status != 0 indicates errors. */
+static void
+exit (int status)
+{
+
+}
+
+/* Runs the executable.  Returns the new process's program id.
+   Must return pid = -1. */
+static pid_t
+exec (const char *cmd_line)
+{
+  return -1;
+}
+
+// static int
+// exec
+
+
+
+
+
+
