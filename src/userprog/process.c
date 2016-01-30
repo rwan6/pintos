a50c3590f53540f52233d42c110a6a10c2a1b8d9
Franky Romero <faromero@usc.edu>
2016-01-30 15:06:25 -0800

    Added "while(1)" to process_wait

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -100,7 +100,7 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  while (1);
+  while(1);
   return -1;
 }
 
@@ -450,7 +450,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE - 12;
+        *esp = PHYS_BASE;
       else
         palloc_free_page (kpage);
     }
