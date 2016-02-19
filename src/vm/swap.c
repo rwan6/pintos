75e150fda55d5e15b48913868071be05784e9ac6
Harvey Han <hanhs@myth4.stanford.edu>
2016-02-19 10:22:06 -0800

    Implemented swap

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,5 +1,46 @@
+#include <bitmap.h>
+#include "vm/frame.h"
+#include "vm/swap.h"
+
+#define BLOCKS_IN_PAGE 8
+
 void
 init_swap_partition (void)
 {
+  struct block *swap_partition = block_get_role (BLOCK_SWAP);
+  swap_bitmap = bitmap_create (block_size (swap_partition));
+  bitmap_set_all (swap_bitmap, false);
+}
 
+void
+swap_read (struct swap_slot* ss, struct frame_entry *fe)
+{
+  int i = 0;
+  for(; i < 8; i++)
+    block_read (swap_partition, ss->sector + i,
+      fe->addr + i * BLOCK_SECTOR_SIZE);
+  swap_free (ss);
+}
+
+void
+swap_write (struct swap_slot* ss, struct frame_entry *fe)
+{
+  swap_allocate (ss);
+  int i = 0;
+  for(; i < 8; i++)
+    block_write (swap_partition, ss->sector + i,
+      fe->addr + i * BLOCK_SECTOR_SIZE);
+}
+
+void
+swap_allocate (struct swap_slot *ss)
+{
+  ASSERT (!bitmap_all (swap_bitmap, 0, block_size (swap_partition)));
+  ss->sector = bitmap_scan_and_flip (swap_bitmap, 0, BLOCKS_IN_PAGE, false);
+}
+
+void
+swap_free (struct swap_slot *ss)
+{
+  bitmap_set_multiple (swap_bitmap, ss->sector, BLOCKS_IN_PAGE, false);
 }
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -2,15 +2,20 @@
 #define VM_SWAP_H
 
 #include "devices/block.h"
-#include "vm/swap.h"
 
 struct swap_slot
 {
   struct page_table_entry *pte;
   block_sector_t sector;
-  struct listelem *swap_elem;
 };
 
-struct bitmap *bitmap;
+struct block *swap_partition;
+struct bitmap *swap_bitmap;
+
+void init_swap_partition (void);
+void swap_read (struct swap_slot *, struct frame_entry *);
+void swap_write (struct swap_slot *, struct frame_entry *);
+void swap_allocate (struct swap_slot *);
+void swap_free (struct swap_slot *);
 
 #endif /* vm/swap.h */
