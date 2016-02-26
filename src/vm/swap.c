cd55d3100cfb4fb7b6b292fcf4a1bd3a0a2e9593
Harvey Han <hanhs@myth32.Stanford.EDU>
2016-02-26 00:21:55 -0800

    Added comments to swap.c

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -3,8 +3,9 @@
 #include "vm/swap.h"
 #include <bitmap.h>
 
-#define BLOCKS_IN_PAGE 8
+#define BLOCKS_IN_PAGE 8  /* Number of block in one page 4K/512. */
 
+/* Initialize the swap partition, corresponding bitmap and lock. */
 void
 init_swap_partition (void)
 {
@@ -14,6 +15,8 @@ init_swap_partition (void)
   lock_init (&swap_lock);
 }
 
+/* Swap in a frame. Read blocks from the swap partition and copy to the
+   physical memory, given the address in the frame entry. */
 void
 swap_read (struct swap_slot* ss, struct frame_entry *fe)
 {
@@ -25,6 +28,8 @@ swap_read (struct swap_slot* ss, struct frame_entry *fe)
   swap_free (ss);
 }
 
+/* Swap out a frame. Copy physical memory and write to blocks in the
+   swap partition, given the address in the frame entry. */
 void
 swap_write (struct swap_slot* ss, struct frame_entry *fe)
 {
@@ -35,15 +40,21 @@ swap_write (struct swap_slot* ss, struct frame_entry *fe)
       fe->addr + i * BLOCK_SECTOR_SIZE);
 }
 
+/* Allocate a swap slot in the swap partition. Set the corresponding
+   bits in the bitmap and record the sector number for bookkeeping. */
 void
 swap_allocate (struct swap_slot *ss)
 {
+  /* Panic the kernel, if we need to swap but the swap is full. */
   ASSERT (!bitmap_all (swap_bitmap, 0, block_size (swap_partition)));
   lock_acquire (&swap_lock);
   ss->sector = bitmap_scan_and_flip (swap_bitmap, 0, BLOCKS_IN_PAGE, false);
   lock_release (&swap_lock);
 }
 
+
+/* Deallocate a swap slot in the swap partition. Reset the corresponding
+   bits in the bitmap to mark it's free. */
 void
 swap_free (struct swap_slot *ss)
 {
